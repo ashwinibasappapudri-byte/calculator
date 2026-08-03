ef798cd95029acc418a238e43618c7e178d4feff
Ashwini <ashwinibasappapudri@gmail.com>
2026-08-03 16:07:09 +0530

    add input2.java

--- input2.java
+++ input2.java
@@ -1,5 +1,5 @@
 public class input2 {
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("hello world that is my first java program");
     }
 }
